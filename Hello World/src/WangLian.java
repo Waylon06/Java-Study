9fde3ba84d14bda9a3877f9569ecb059740d381b
Waylon <205218290@qq.com>
2021-09-24 22:47:32 +0800

    1.0.0

---
+++ Hello World/src/WangLian.java
@@ -0,0 +1,23 @@
+public class WangLian {
+    public String name;   //名字
+
+    public static void main(String[] args) {
+        eat();
+
+
+        travel();
+        hair();
+    }
+
+    public static void eat(){
+        System.out.println("喜欢吃好吃的");
+    }
+
+    public static void travel(){
+        System.out.println("喜欢旅游");
+    }
+
+    public static void hair(){
+        System.out.println("不想秃头");
+    }
+}
