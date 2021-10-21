18044434ed6bb2cb825c51764c43d8f0fafed13a
Waylon <205218290@qq.com>
2021-10-21 23:57:50 +0800

    1.0.4

---
+++ 赋值类运算符/src/Calculate.java
@@ -0,0 +1,11 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int i=10;
+        i*=5;
+        System.out.println(i);
+        i/=5;
+        System.out.println(i);
+        i%=2;
+        System.out.println(i);
+    }
+}
